52f068d5e025f43ef58c5606eee851fa2f822047
James Reed <james.reed@startnet.co.uk>
2016-12-16 11:47:46 +0000

    update of test.php again

--- other/test.php
+++ other/test.php
@@ -1,13 +1,9 @@
 <?php
-<<<<<<< HEAD
 echo "hello";
 // james master update here
 ?>
-=======
 if (1==1) { echo "WORD"; }
 ?>
 echo "helloNOPE";
-echo "this using the sprouts branch";
+echo "this using the master branch";
 ?>
-// changed this file
->>>>>>> 6869bf7888e0ff38298fecf1e0191d8a24d036e0
