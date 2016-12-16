7d10f14fee6b91ded54495cb6c73321bbcacdcf6
James Reed <james.reed@startnet.co.uk>
2016-12-16 11:48:45 +0000

    yet more updating of test.php

--- other/test.php
+++ other/test.php
@@ -1,7 +1,10 @@
 <?php
 if (1==1) { echo "WORD"; }
+if (2==1) { echo "WORD OUT"; }
+
 ?>
 echo "helloNOPE";
-echo "this using the sprouts branch";
+echo "this using the sprouts YES branch";
+echo "so I'm adding this sprouts line here";
 ?>
 // changed this file
