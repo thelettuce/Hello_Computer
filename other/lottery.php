0d99247dc33bf06584cd53afda5f8f5c31f4c373
James Reed <james.reed@startnet.co.uk>
2016-12-16 12:20:16 +0000

    updated here

--- other/lottery.php
+++ other/lottery.php
@@ -1,13 +1,7 @@
 Coolio
 <?php
-<<<<<<< HEAD
 echo "And this";
 echo "I WON MASTER THE LOTTERY"; // possible this too from master
 echo "I've aded this using MASTER";
 // alright
-=======
-// some extra comment here
-echo "I WON THE LONIONS-TTERY";
-// I've added this using the onions branch
->>>>>>> onions
 ?>
