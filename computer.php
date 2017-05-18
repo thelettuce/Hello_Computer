b3541883384bd7ad12d84bd633aff90fd1b7f651
James Reed <james.reed@startnet.co.uk>
2017-05-18 17:09:04 +0100

    update by JR3 to computer.php

--- computer.php
+++ computer.php
@@ -2,7 +2,9 @@ HELLO COMPUTER
 <?php
 echo "WORD<br>";
 echo "LATEST CHANGE";
+echo "PUT HERE BY JR3";
 echo "TESTINGAGAIN";
 echo "FIXED";
+echo "I UPDATED THIS USING JR3";
 // another update here by James
 ?>
