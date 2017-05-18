1d5757eb023dc1ada10efc5c022745daa6065ae9
James Reed <james.reed@startnet.co.uk>
2017-05-18 17:20:05 +0100

    more lines from jr2

--- computer.php
+++ computer.php
@@ -5,8 +5,10 @@ echo "WORD<br>";
 echo "LATEST CHANGE - DONE BY JR2 HERE";
 echo "PUT HERE BY JR3";
 echo "JR2 FIX";
-echo "TESTINGAGAIN";
+echo "TESTINGAGAIN ANOTHER ADD BY JR2";
 echo "FIXED";
+echo "EVERY JR2 LATEST";
+
 echo "I UPDATED THIS USING JR3";
 // another update here by James
 ?>
